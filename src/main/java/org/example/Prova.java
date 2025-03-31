9f618e0216d108dc7e752fdc401631b3ed15df69
chiarabeccarini <beccarini.chiara@gmail.com>
2025-03-31 12:16:23 +0200

    modifica di prova per testare il CI

--- src/main/java/org/example/Prova.java
+++ src/main/java/org/example/Prova.java
@@ -3,6 +3,6 @@ package org.example;
 public class Prova {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ciao Mondo!");
     }
 }
